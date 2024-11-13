c143fe799cd7267bcd42a7490e99e45686f1c82a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-13 19:29:34 +0100

    adding authZ to emails route

--- routes/emails.js
+++ routes/emails.js
@@ -2,21 +2,22 @@ const express = require('express')
 const emailsRouter = express.Router()
 const emailController = require('../controllers/email.controller')
 const requireAuth = require('../lib/require-auth')
+const enforce = require('../lib/enforce')
 
 
 // let upload = multer({
 //     dest: path.join(__dirname, '../uploads')
 // })
 
-// emailsRouter.use(requireAuth)
+emailsRouter.use(requireAuth)
 
 emailsRouter.route('/')
     .get(emailController.getEmailsRoute)
     .post(emailController.upload.array('file'), emailController.createEmailRoute)
 emailsRouter.route('/:id')
     .get(emailController.getEmailRoute)
-    .put(emailController.updateEmailRoute)
-    .delete(emailController.deleteEmailRoute)
+    .put(enforce(emailController.updateEmailPolicy), emailController.updateEmailRoute)
+    .delete(enforce(emailController.deleteEmailPolicy), emailController.deleteEmailRoute)
 
 
 module.exports = emailsRouter
